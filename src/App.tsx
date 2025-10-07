b3cec21b79e59f151239ed90b68e4259d425c2d8
chamithsandeepa <chamithsandeepa321@gmail.com>
2025-10-07 15:11:55 +0530

    Add new sections for FlowSuite, Admin Control, and various reporting features in App component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,22 @@
 import { useState, useEffect } from "react";
 import Navigation from "./components/Navigation";
 import HeroSection from "./components/HeroSection";
-import FeatureSection from "./components/FeatureSection";
-import WorkflowSection from "./components/WorkflowSection";
 import KeyFeaturesSection from "./components/KeyFeaturesSection";
-import PlansSection from "./components/PlansSection";
-import CallToActionSection from "./components/CallToActionSection";
+// import PlansSection from "./components/PlansSection";
+// import CallToActionSection from "./components/CallToActionSection";
 import Footer from "./components/Footer";
+import FlowSuiteSection from "./components/FlowSuite";
+import AdminControlSection from "./components/AdminControll";
+import Steps from "./components/Steps";
+import InventoryReportingSection from "./components/Inventory";
+import StockTransfer from "./components/StockTransfer";
+import Automated from "./components/Automated";
+import DamageFlowSection from "./components/DamageFlow";
+import AdvancedPricingSection from "./components/PricingSystem";
+import DTForder from "./components/DTForder";
+import Plans from "./components/Plans";
+import FAQSection from "./components/FAQ";
+import CTASection from "./components/ContactUs";
 
 const App = () => {
   const [activeSection, setActiveSection] = useState("home");
@@ -60,11 +70,21 @@ const App = () => {
         scrollToSection={scrollToSection}
       />
       <HeroSection scrollToSection={scrollToSection} />
-      <FeatureSection />
-      <WorkflowSection />
+      <FlowSuiteSection />
+      <AdminControlSection />
+      <Steps />
       <KeyFeaturesSection />
-      <PlansSection scrollToSection={scrollToSection} />
-      <CallToActionSection scrollToSection={scrollToSection} />
+      <InventoryReportingSection />
+      <StockTransfer />
+      <Automated />
+      <DamageFlowSection />
+      <AdvancedPricingSection />
+      <DTForder />
+      <Plans />
+      <FAQSection />
+      <CTASection />
+      {/* <PlansSection scrollToSection={scrollToSection} /> */}
+      {/* <CallToActionSection scrollToSection={scrollToSection} /> */}
       <Footer scrollToSection={scrollToSection} />
     </div>
   );
